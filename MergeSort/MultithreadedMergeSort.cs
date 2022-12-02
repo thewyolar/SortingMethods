f52fd14d97140c6ccea971daa960bafb7bee81a6
thewyolar <thewyolar@gmail.com>
2022-12-02 20:48:39 +0300

    Add merge sort project.

---
+++ MergeSort/MultithreadedMergeSort.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Numerics;
+using System.Threading;
+
+namespace MergeSort;
+
+public class MergeSort
+{
+    public static void MergeSortSequential<T>(T[] arr) where T : INumber<T>
+    {
+        MergeSortSequential(arr, 0, arr.Length - 1);
+    }
+
+    public static void MultithreadedMergeSort<T>(T[] arr, int mtCount) where T : INumber<T>
+    {
+        MultithreadedMergeSort(arr, 0, arr.Length - 1, mtCount);
+    }
+
+    private static void MultithreadedMergeSort<T>(T[] arr, int lowIndex, int highIndex, int mtCount) where T : INumber<T>
+    {
+        int middleIndex = (lowIndex + highIndex) / 2;
+
+        if (lowIndex < highIndex)
+        {
+            if (mtCount > 1)
+            {
+                Thread t = new Thread(() => MultithreadedMergeSort(arr, lowIndex, middleIndex, mtCount - 1));
+                t.Start();
+                
+                MultithreadedMergeSort(arr, middleIndex + 1, highIndex, mtCount - 1);
+
+                t.Join();
+            }
+            else
+            {
+                MergeSortSequential(arr, lowIndex, middleIndex);
+                MergeSortSequential(arr, middleIndex + 1, highIndex);
+            }
+
+            Merge(arr, lowIndex, middleIndex, highIndex);
+        }
+    }
+
+    private static void MergeSortSequential<T>(T[] arr, int lowIndex, int highIndex) where T : INumber<T>
+    {
+        if (lowIndex < highIndex)
+        {
+            int middleIndex = (lowIndex + highIndex) / 2;
+            MergeSortSequential(arr, lowIndex, middleIndex);
+            MergeSortSequential(arr,middleIndex + 1, highIndex);
+            Merge(arr, lowIndex, middleIndex, highIndex);
+        }
+    }
+    
+    private static void Merge<T>(T[] arr, int lowIndex, int middleIndex, int highIndex) where T : INumber<T>
+    {
+        int left = lowIndex;
+        int right = middleIndex + 1;
+        T[] temp = new T[highIndex - lowIndex + 1];
+        int index = 0;
+
+        while (left <= middleIndex && right <= highIndex)
+        {
+            if (arr[left] < arr[right])
+            {
+                temp[index] = arr[left];
+                left++;
+            }
+            else
+            {
+                temp[index] = arr[right];
+                right++;
+            }
+
+            index++;
+        }
+
+        for (int i = left; i <= middleIndex; i++)
+        {
+            temp[index] = arr[i];
+            index++;
+        }
+
+        for (int i = right; i <= highIndex; i++)
+        {
+            temp[index] = arr[i];
+            index++;
+        }
+
+        for (int i = 0; i < temp.Length; i++)
+            arr[lowIndex + i] = temp[i];
+    }
+}
---
+++ MergeSort/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using static MergeSort.MergeSort;
+
+namespace MergeSort;
+
+class Program
+{
+    public static int[] Generate(int size)
+    {
+        int[] result = new int[size];
+        for (int i = 0; i < result.Length; i++)
+            result[i] = new Random().Next(0, size);
+
+        return result;
+    }
+
+
+    static void Main(string[] args)
+    {
+        Stopwatch sw = new Stopwatch();
+        int[] test1 = Generate(10000000);
+        sw.Start();
+        //MergeSortSequential(test1);
+        MultithreadedMergeSort(test1, 6);
+        sw.Stop();
+        Console.WriteLine("Время, затраченное на выполнение: " + sw.ElapsedMilliseconds + "ms");
+    }
+}
