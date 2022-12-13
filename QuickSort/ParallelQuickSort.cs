970c8b9dda27b4d4c3d36c4559e880502c5ccc50
thewyolar <thewyolar@gmail.com>
2022-12-13 19:35:06 +0300

    add: documentation for quicksort algorithms.

--- QuickSort/ParallelQuickSort.cs
+++ QuickSort/ParallelQuickSort.cs
@@ -2,18 +2,39 @@
 
 namespace QuickSort;
 
-public class ParallelQuickSort
+public static class ParallelQuickSort
 {
-    public static void QuickSortSequential<T>(T [] arr) where T : INumber<T>
+    /// <summary>
+    /// Запускает последовательную быструю сортировку.
+    /// </summary>
+    /// <param name="arr">сортируемый массив</param>
+    /// <typeparam name="T">тип элементов массива</typeparam>
+    public static void QuickSortSequential<T>(T[] arr) where T : INumber<T>
     {
         QuickSortSequential(arr, 0, arr.Length - 1);
     }
     
+    /// <summary>
+    /// Запускает параллельную быструю сортировку. 
+    /// </summary>
+    /// <param name="arr">сортируемый массив.</param>
+    /// <typeparam name="T">тип элементов массива.</typeparam>
     public static void QuickSortParallel<T>(T[] arr) where T : INumber<T>
     {
         QuickSortParallel(arr, 0, arr.Length - 1);
     }
     
+    /// <summary>
+    /// Перегруженный метод последовательной быстрой сортировки.
+    /// Осуществляет выбор опорного элемента в массиве.
+    /// Разбиение: перераспределяет элементы в массиве таким образом, что элементы,
+    /// меньшие опорного, помещаются перед ним, а большие или равные - после.
+    /// Рекурсивно применяет первые два шага к двум подмассивам слева и справа от опорного элемента.
+    /// </summary>
+    /// <param name="arr">сортируемый массив.</param>
+    /// <param name="left">нижняя граница сортируемого массива.</param>
+    /// <param name="right">верхняя граница сортируемого массива.</param>
+    /// <typeparam name="T">тип элементов массива.</typeparam>
     private static void QuickSortSequential<T>(T[] arr, int left, int right) where T : INumber<T>
     {
         if (right > left)
@@ -24,12 +45,20 @@ public class ParallelQuickSort
         }
     }
 
+    /// <summary>
+    /// Перегруженный метод параллельной быстрой сортировки.
+    /// Параллельно запускает рекурсию к двум подмассивам слева и справа от опорного элемента.
+    /// </summary>
+    /// <param name="arr">сортируемый массив.</param>
+    /// <param name="left">нижняя граница сортируемого массива.</param>
+    /// <param name="right">верхняя граница сортируемого массива.</param>
+    /// <typeparam name="T">тип элементов массива.</typeparam>
     private static void QuickSortParallel<T>(T[] arr, int left, int right) where T : INumber<T>
     {
-        const int SEQUENTIAL_THRESHOLD = 2048;
+        const int sequentialThreshold = 2048;
         if (right > left)
         {
-            if (right - left < SEQUENTIAL_THRESHOLD)
+            if (right - left < sequentialThreshold)
             {
                 QuickSortSequential(arr, left, right);
             }
@@ -43,13 +72,26 @@ public class ParallelQuickSort
         }
     }
 
+    /// <summary>
+    /// Меняет местами два элемента массива.
+    /// </summary>
+    /// <param name="arr">массив, элементы которого меняются местами.</param>
+    /// <param name="i">индекс первого элемента.</param>
+    /// <param name="j">индекс второго элемента.</param>
+    /// <typeparam name="T">тип элементов массива.</typeparam>
     private static void Swap<T>(T[] arr, int i, int j) where T : INumber<T>
     {
-        T tmp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = tmp;
+        (arr[i], arr[j]) = (arr[j], arr[i]);
     }
 
+    /// <summary>
+    /// Осуществляет выбор опорного элемента в массиве.
+    /// </summary>
+    /// <param name="arr">сортируемый массив.</param>
+    /// <param name="low">нижняя граница сортируемого массива.</param>
+    /// <param name="high">верхняя граница сортируемого массива.</param>
+    /// <typeparam name="T">тип элементов массива.</typeparam>
+    /// <returns>Возвращает индекс опорного элемента массива.</returns>
     private static int Partition<T>(T[] arr, int low, int high) where T : INumber<T>
     {
         int pivotPos = (high + low) / 2;
